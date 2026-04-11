20aee95787a238b37bb9269d9e716b4731a9f30d
ChangHyeon Lee <canghaun@gmail.com>
2026-04-11 22:45:51 +0900

    :sparkles: Feat: Board의 빈 곳에 새 Block을 추가하는 기능 추가

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,15 +1,53 @@
 import styles from "./GamePage.module.css";
 import Board from "../components/Board";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const GamePage = () => {
   const [board, setBoard] = useState([
-    [0, 2, 4, 8],
-    [16, 32, 64, 128],
-    [256, 512, 1024, 2048],
-    [5096, 10192, 20384, 40768],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
   ]);
   const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const empty = [];
+    board.forEach((row, r) =>
+      row.forEach((cell, c) => {
+        empty.push([r, c]);
+      }),
+    );
+    const [i1, j1] = empty.splice(
+      Math.floor(Math.random() * empty.length),
+      1,
+    )[0];
+    const [i2, j2] = empty[Math.floor(Math.random() * empty.length)];
+    setBoard(
+      board.map((row, r) =>
+        row.map((cell, c) =>
+          (r === i1 && c === j1) || (r === i2 && c === j2) ? 2 : cell,
+        ),
+      ),
+    );
+  }, []);
+
+  const addNewBlock = () => {
+    const empty = [];
+    board.forEach((row, r) =>
+      row.forEach((cell, c) => {
+        if (cell === 0) empty.push([r, c]);
+      }),
+    );
+    const [i, j] = empty[Math.floor(Math.random() * empty.length)];
+    setBoard(
+      board.map((row, r) =>
+        row.map((cell, c) =>
+          r === i && c === j ? (Math.floor(Math.random() * 2) + 1) * 2 : cell,
+        ),
+      ),
+    );
+  };
 
   return (
     <div className={styles.page}>
