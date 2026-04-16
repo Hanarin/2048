64a71de1153855fe370cb143de8b6f3f51dc1a4a
ChangHyeon Lee <canghaun@gmail.com>
2026-04-17 01:50:08 +0900

    :bug: Fix: 같은 블록이 3개 있을 경우 진행방향쪽의 2개의 블록이 합쳐지도록 수정

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -26,6 +26,9 @@ const GamePage = () => {
     }
   };
 
+  // TODO: 같은 블럭이 3개가 겹쳐있을 때 이동 방향에 따라 합쳐지는 블럭이 달라지도록 수정하기
+  // [2, 2, 2, 0]을 왼쪽으로 이동할 경우 [4, 2, 0, 0]
+  // [2, 2, 2, 0]을 오른쪽으로 이동할 경우 [0, 0, 2, 4]
   const moveBlocks = (state, direction) => {
     if (["ArrowUp", "ArrowDown"].includes(direction)) {
       let score = 0;
@@ -37,7 +40,11 @@ const GamePage = () => {
         for (let j = 0; j < state.board.length; j++) {
           column.push(state.board[j][i]);
         }
-        const { mergedArr, scoreGained } = merge(column);
+        const { mergedArr, scoreGained } =
+          direction === "ArrowUp"
+            ? merge(column)
+            : merge([...column].reverse());
+        if (direction === "ArrowDown") mergedArr.reverse();
         score += scoreGained;
         for (let c = 0; c < state.board.length; c++) {
           if (direction === "ArrowUp") {
@@ -55,7 +62,9 @@ const GamePage = () => {
     } else if (["ArrowLeft", "ArrowRight"].includes(direction)) {
       let score = 0;
       const newBoard = state.board.map((row) => {
-        const { mergedArr, scoreGained } = merge(row);
+        const { mergedArr, scoreGained } =
+          direction === "ArrowLeft" ? merge(row) : merge([...row].reverse());
+        if (direction === "ArrowRight") mergedArr.reverse();
         score += scoreGained;
         const zeros = Array.from(
           { length: row.length - mergedArr.length },
@@ -115,6 +124,16 @@ const GamePage = () => {
   };
 
   const initialState = createInitialState();
+
+  // const initialState = {
+  //   board: [
+  //     [0, 0, 0, 0],
+  //     [2, 0, 0, 0],
+  //     [2, 0, 0, 0],
+  //     [2, 0, 0, 0],
+  //   ],
+  //   score: 0,
+  // };
 
   const reducer = (state, action) => {
     switch (action.type) {
